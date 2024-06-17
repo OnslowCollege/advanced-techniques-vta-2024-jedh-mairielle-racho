73a3f16f3a2280f2997057a41f21d643a1cc112e
Jedh <100874710+jedhr@users.noreply.github.com>
2024-06-17 06:14:19 +0000

    Jun 17, 2024, 6:14 AM

--- settings.py
+++ settings.py
@@ -1,1 +1,14 @@
-"""
+"""Program settings."""
+import pygame
+
+# screen
+SCREEN_W: int = 480
+SCREEN_H: int = 800
+
+# screen
+class Screen:
+    """Screen parent class."""
+
+    def __init__(self) -> None:
+        """Initialise."""
+        self.
