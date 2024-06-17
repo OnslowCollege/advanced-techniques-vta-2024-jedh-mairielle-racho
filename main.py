4b6090a8dbd7c03c220b94a48b4dd6203c38f7ad
Jedh <100874710+jedhr@users.noreply.github.com>
2024-06-17 06:47:55 +0000

    Jun 17, 2024, 6:47 AM

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 """
-Main.
+Play Blackjack with an Onslow Twist.
 
 Created by: Jedh
 Date: 2024-06-17
 """
+import pygame
+from settings import*
 
 # run main
 class Main:
@@ -11,4 +13,23 @@ class Main:
 
     def __init__(self) -> None:
         """Initialise."""
-        pass
+        pygame.init()
+        self.run: bool = True
+
+        # create display
+        self.screen = pygame.display.setmode((SCREEN_W, SCREEN_H),
+        pygame.RESIZABLE)
+        pygame.display.set_caption("Blackjack")  # name window
+
+    def event_handler(self) -> None:
+        """Handle user-interaction events."""
+        pygame.display.update()
+        for event in pygame.event.get():
+            # if user quits program window
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def run(self) -> None:
+        while self.run == True:
+            
--- settings.py
+++ settings.py
@@ -5,24 +5,9 @@ import pygame
 SCREEN_W: int = 480
 SCREEN_H: int = 800
 
-# screen
-class Screen:
-    """Screen parent class."""
+# framerate
+FPS: int = 60
 
-    def __init__(self) -> None:
-        """Initialise."""
-        self.run: bool = True
+# colours
 
-        # create display
-        self.screen = pygame.display.setmode((SCREEN_W, SCREEN_H),
-        pygame.RESIZABLE)
-        pygame.display.set_caption("Blackjack")  # name window
-
-    def event_handler(self) -> None:
-        """Handle user-interaction events."""
-        pygame.display.update()
-        for event in pygame.event.get():
-            # if user quits program window
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+# font
