0aaa9b49f7fa9f128a2d281d8628fc614eb46b98
Jedh <100874710+jedhr@users.noreply.github.com>
2024-06-17 07:28:45 +0000

    Jun 17, 2024, 7:28 AM

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ class Main:
         self.run_display: bool = True
 
         # create display
-        self.screen = pygame.display.set_mode((SCREEN_W, SCREEN_H))
+        self.screen = pygame.display.set_mode(
+            (SCREEN_W, SCREEN_H), pygame.RESIZABLE
+        )
         pygame.display.set_caption("Blackjack")  # name window
         self.display_surf = pygame.display.get_surface()
 
