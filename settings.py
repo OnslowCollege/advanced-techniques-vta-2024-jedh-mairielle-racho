64b101cd0f8ce90e221df11326ada462f03e92f7
Jedh <100874710+jedhr@users.noreply.github.com>
2024-06-17 07:00:41 +0000

    Jun 17, 2024, 7:00 AM

--- settings.py
+++ settings.py
@@ -9,5 +9,17 @@ SCREEN_H: int = 800
 FPS: int = 60
 
 # colours
+WHITE: str = "#f7f6f6"
+L_GREEN: str = "#7ec4a0"
+D1_GREEN: str = "#266b63"
+D2_GREEN: str = "163e39"
+RED: str = "#b63110"
 
 # font
+def p_font(size: int) -> pygame.font.Font:
+    """Get the primary font."""
+    return pygame.font.Font("assets/font/Makasih.ttf", size)
+
+def s_font(size: int) -> pygame.font.Font:
+    """Get secondary font."""
+    return pygame.font.Font("assets/font/TangoSans.ttf", size)
