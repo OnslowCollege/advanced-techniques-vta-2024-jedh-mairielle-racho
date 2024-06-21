3f14a30f2382cb8897a8a8ec96631382f7966aa2
Jedh <100874710+jedhr@users.noreply.github.com>
2024-06-21 20:04:05 +1200

    Program: Trial a different programming technique.
    - Attempt to streamline files to most necessary groupings.
    - 'Client' file created where program will be executed.

---
+++ client.py
@@ -0,0 +1,17 @@
+"""
+Play Blackjack with an Onslow Twist.
+
+Created by: Jedh
+Date: 2024-06-17
+"""
+
+import pygame
+
+
+# create the program
+class Program:
+    """The program to play Blackjack."""
+
+    # initiator method
+    def __init__(self) -> None:
+        """Initialise the program."""
